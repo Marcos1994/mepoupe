db754c476bc0846d5b923f246d3bd233a55092f8
Marcos1994 <marcos.antas@hotmail.com.br>
2014-08-07 17:31:10 -0300

    .

--- MePoupe/app/models/entry.rb
+++ MePoupe/app/models/entry.rb
@@ -1,4 +1,20 @@
 class Entry < ActiveRecord::Base
 	has_many :parts, dependent: :destroy
 	belongs_to :ctegory
+	after_create :criar_parcelas
+	
+	def criar_parcelas(dados)
+		if(periodicidade != 1)
+			dados.parcelas = 1
+		end
+		
+		for i in (0..(dados.parcelas)) do
+			self.parts
+			dados.data = proximo_mes(dados.data)
+		end
+	end
+	
+	def proximo_mes(mes_atual)
+		dt_at = /(....).(..).(..)/.match(Date.today.to_s)
+	end
 end
