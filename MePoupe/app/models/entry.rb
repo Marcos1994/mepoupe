b6ec969c216a0c7d3d28362d3d5ff8cd3899c5c5
MarcosAndre <marcos.antas@hotmail.com.br>
2014-08-24 23:52:43 -0300

    Metodos de Entry

--- MePoupe/app/models/entry.rb
+++ MePoupe/app/models/entry.rb
@@ -9,4 +9,30 @@ class Entry < ActiveRecord::Base
 		end
 		@valor
 	end
+	
+	def valor_efetivado
+		self.valor(1)
+	end
+	
+	def valor_pendente
+		self.valor(0)
+	end
+	
+	def valor_razao_efetivado
+		((self.valor_efetivado * 100) / self.valor_total).to_i
+	end
+	
+	def valor_razao_pendente
+		((self.valor_pendente * 100) / self.valor_total).to_i
+	end
+	
+	def valor(estado)
+		@valor = 0
+		self.parts.each do |p|
+			if(p.confirmacao == estado)
+				@valor += p.valor
+			end
+		end
+		@valor
+	end
 end
