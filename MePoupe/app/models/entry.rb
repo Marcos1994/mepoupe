b2654e33f1e03157e22bbf5826de0ae777feb26f
MarcosAndre <marcos.antas@hotmail.com.br>
2014-08-24 23:13:05 -0300

    valor total

--- MePoupe/app/models/entry.rb
+++ MePoupe/app/models/entry.rb
@@ -1,4 +1,12 @@
 class Entry < ActiveRecord::Base
 	has_many :parts, dependent: :destroy, autosave: true
 	belongs_to :category
+	
+	def valor_total
+		@valor = 0
+		self.parts.each do |p|
+			@valor += p.valor
+		end
+		@valor
+	end
 end
