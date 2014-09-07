ac36fdd06b01b99645363aac8264eb93bc7ad3e5
deecc <decio.crf@gmail.com>
2014-09-07 12:17:26 -0300

    Alterações em metas e index

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,6 +1,22 @@
 class Goal < ActiveRecord::Base
 
 	belongs_to :user
-	has_one :category
+	belongs_to :category
 
+	before_save :default_state_value	
+
+	
+	def default_state_value
+		if self.fim >= Date.today
+			self.estado = 1
+		else self.estado = 0
+		end
+	end
+
+	def contaDias
+		@dias = self.fim - Date.current
+		@dias.to_i
+	end
+
+	
 end
